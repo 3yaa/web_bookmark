90cc0b10f1582d6e7f881f546ee54361fd78b606
3yaa <3yaa@users.noreply.github.com>
2025-11-10 20:27:00 -0800

    next fix

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6ammLFovFTh6UVQvUEBxSAPXuVUO4gss10Z093Ezuu8=\"\n}"
--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__816de575._.css",
-  "static/chunks/node_modules_4d97f8fc._.js",
-  "static/chunks/_b692e06e._.js"
-],
-    source: "dynamic"
-});

b7a6952d497c1636306da86bb907ff38977ed867
3yaa <93301444+3yaa@users.noreply.github.com>
2025-11-29 17:03:00 -0800

    idk man

--- app/movies/components/detailViews/MovieMobileDetails.tsx
+++ app/movies/components/detailViews/MovieMobileDetails.tsx
@@ -66,7 +66,7 @@ export function MovieMobileDetails({
         </div>
       )}
       {/* INFO */}
-      <div className="flex-1 min-h-[100dvh] mb-10">
+      <div className="flex-1 min-h-[100dvh] mb-50">
         {/* POSTER */}
         <div className="relative w-full overflow-hidden bg-zinc-900/40">
           {movie.posterUrl ? (

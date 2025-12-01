4dbf9fc6487ef12d17c9b31fb6f933704744501a
3yaa <3yaa@users.noreply.github.com>
2025-11-30 18:50:51 -0800

    season test

--- app/shows/components/detailsViews/ShowMobileDetails.tsx
+++ app/shows/components/detailsViews/ShowMobileDetails.tsx
@@ -154,203 +154,207 @@ export function ShowMobileDetails({
   };
 
   return (
-    <div
-      ref={modalRef}
-      className={`fixed inset-0 z-30 bg-zinc-950 flex flex-col ${
-        isProgressPickerOpen ? "overflow-hidden" : "overflow-y-auto"
-      }`}
-      style={{
-        transform: `translateY(${translateY}px)`,
-        opacity: isVisible ? 1 : 0,
-        transition: isDragging
-          ? "none"
-          : isExiting
-          ? "transform 0.3s cubic-bezier(0.4, 0, 0.2, 1), opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1)"
-          : "transform 0.4s cubic-bezier(0.16, 1, 0.3, 1), opacity 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
-      }}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      {isLoading?.isTrue && (
-        <Loading
-          customStyle={isLoading.style}
-          text={isLoading.text}
-          isMobile={true}
-        />
-      )}
-      {/* ACTION BAR */}
-      {(posterLoaded || addingShow) && (
-        <div className="sticky top-0 z-30">
-          <div className="absolute top-0 left-0 right-0 px-4 py-3 flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              {addingShow && (
-                <>
-                  <button
-                    className="bg-zinc-800/50 backdrop-blur-2xl p-2 rounded-md px-2.5 active:scale-95 transition-transform duration-150"
-                    onClick={() => {
-                      onAction({ type: "needYear" });
-                    }}
-                    title={"Search with year"}
-                  >
-                    <ChevronsUp className="w-5 h-5 text-slate-400 transition-colors" />
-                  </button>
-                  <button
-                    className="bg-zinc-800/50 backdrop-blur-2xl p-2 rounded-md active:scale-95 transition-transform duration-150"
-                    onClick={onAddShow}
-                  >
-                    <Plus className="w-5 h-5 text-slate-400" />
-                  </button>
-                </>
-              )}
+    <>
+      <div
+        ref={modalRef}
+        className={`fixed inset-0 z-30 bg-zinc-950 flex flex-col ${
+          isProgressPickerOpen ? "overflow-hidden" : "overflow-y-auto"
+        }`}
+        style={{
+          transform: `translateY(${translateY}px)`,
+          opacity: isVisible ? 1 : 0,
+          transition: isDragging
+            ? "none"
+            : isExiting
+            ? "transform 0.3s cubic-bezier(0.4, 0, 0.2, 1), opacity 0.3s cubic-bezier(0.4, 0, 0.2, 1)"
+            : "transform 0.4s cubic-bezier(0.16, 1, 0.3, 1), opacity 0.3s cubic-bezier(0.16, 1, 0.3, 1)",
+        }}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {isLoading?.isTrue && (
+          <Loading
+            customStyle={isLoading.style}
+            text={isLoading.text}
+            isMobile={true}
+          />
+        )}
+        {/* ACTION BAR */}
+        {(posterLoaded || addingShow) && (
+          <div className="sticky top-0 z-30">
+            <div className="absolute top-0 left-0 right-0 px-4 py-3 flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                {addingShow && (
+                  <>
+                    <button
+                      className="bg-zinc-800/50 backdrop-blur-2xl p-2 rounded-md px-2.5 active:scale-95 transition-transform duration-150"
+                      onClick={() => {
+                        onAction({ type: "needYear" });
+                      }}
+                      title={"Search with year"}
+                    >
+                      <ChevronsUp className="w-5 h-5 text-slate-400 transition-colors" />
+                    </button>
+                    <button
+                      className="bg-zinc-800/50 backdrop-blur-2xl p-2 rounded-md active:scale-95 transition-transform duration-150"
+                      onClick={onAddShow}
+                    >
+                      <Plus className="w-5 h-5 text-slate-400" />
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      {/* INFO */}
-      <div className="pb-10">
-        {/* POSTER */}
-        <div
-          className={`relative w-full overflow-hidden bg-zinc-900/40 transition-all duration-300 ${
-            isDragging && "rounded-lg"
-          }`}
-        >
-          {show.posterUrl ? (
-            <Image
-              src={show.posterUrl}
-              alt={show.title || "Poster"}
-              width={1280}
-              height={900}
-              className="object-cover w-full"
-              onLoad={() => setPosterLoaded(true)}
-            />
-          ) : (
-            <div className="h-64 bg-gradient-to-br from-zinc-700 to-zinc-800" />
-          )}
+        )}
+        {/* INFO */}
+        <div className="pb-10">
+          {/* POSTER */}
+          <div
+            className={`relative w-full overflow-hidden bg-zinc-900/40 transition-all duration-300 ${
+              isDragging && "rounded-lg"
+            }`}
+          >
+            {show.posterUrl ? (
+              <Image
+                src={show.posterUrl}
+                alt={show.title || "Poster"}
+                width={1280}
+                height={900}
+                className="object-cover w-full"
+                onLoad={() => setPosterLoaded(true)}
+              />
+            ) : (
+              <div className="h-64 bg-gradient-to-br from-zinc-700 to-zinc-800" />
+            )}
 
-          <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-zinc-950 to-transparent pointer-events-none" />
-        </div>
+            <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-zinc-950 to-transparent pointer-events-none" />
+          </div>
 
-        <div className="px-4">
-          <div className="mt-4">
-            <div></div>
-            <div className="flex justify-between">
-              <h1 className="text-zinc-100 text-2xl font-bold -mt-0.5">
-                {show.title}
-              </h1>
-              <div data-no-drag>
-                <MobilePicker
-                  score={show.score || 0}
-                  onScoreChange={(newScore) =>
+          <div className="px-4">
+            <div className="mt-4">
+              <div></div>
+              <div className="flex justify-between">
+                <h1 className="text-zinc-100 text-2xl font-bold -mt-0.5">
+                  {show.title}
+                </h1>
+                <div data-no-drag>
+                  <MobilePicker
+                    score={show.score || 0}
+                    onScoreChange={(newScore) =>
+                      onAction({
+                        type: "changeScore",
+                        payload: newScore,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+              <div className="text-zinc-400 text-sm -mt-1 flex items-center gap-2">
+                <span>{show.studio || "Unknown"}</span>•
+                <span>{show.dateReleased || "-"}</span>
+                {show.dateCompleted && (
+                  <>
+                    •<span>{formatDateShort(show.dateCompleted)}</span>
+                  </>
+                )}
+              </div>
+            </div>
+            {/* PROGRESS BAR */}
+            <div
+              className="mt-4.5 w-full bg-zinc-800/80 rounded-md h-1.5 overflow-hidden"
+              onClick={() => setIsProgressPickerOpen(true)}
+            >
+              <div
+                className={`${getStatusBg(
+                  show.status
+                )} h-1.5 transition-all duration-500 ease-out rounded-md`}
+                style={{
+                  width: `${
+                    show.seasons?.[show.curSeasonIndex]?.episode_count
+                      ? calcCurProgress(
+                          show.seasons,
+                          show.curSeasonIndex,
+                          show.curEpisode
+                        )
+                      : 0
+                  }%`,
+                }}
+              />
+            </div>
+            {/* SEASON / EPISODES */}
+            <div className="mt-1 flex justify-between text-zinc-400 text-sm font-bold mb-0.5">
+              <span onClick={() => setIsProgressPickerOpen(true)}>
+                Season: {show.curSeasonIndex + 1 || "-"}
+              </span>
+              <span onClick={() => setIsProgressPickerOpen(true)}>
+                Episode: {show.curEpisode || "-"}
+              </span>
+            </div>
+            {/* STATUS */}
+            <div className="mt-2.5" data-no-drag>
+              <label className="text-zinc-400 text-xs font-medium">
+                Status
+              </label>
+              <div className="pt-1 flex flex-wrap gap-2 pb-1">
+                {showStatusOptions.map((status, index) => (
+                  <button
+                    key={status.value}
+                    onClick={() =>
+                      onAction({
+                        type: "changeStatus",
+                        payload: `${status.label}`,
+                      })
+                    }
+                    className={`${
+                      index === 3 ? "w-full" : "flex-1"
+                    } px-4 py-1.5 text-sm rounded-md border border-zinc-700/30 font-semibold whitespace-nowrap transition-all duration-200 active:scale-95 ${
+                      status.label === show.status
+                        ? `${getStatusBg(status.label)} text-zinc-100`
+                        : "text-zinc-300 bg-zinc-900/40 hover:bg-zinc-800/60"
+                    }`}
+                  >
+                    {status.label}
+                  </button>
+                ))}
+              </div>
+            </div>
+            {/* NOTE */}
+            <div className="mt-1" data-no-drag>
+              <label className="text-zinc-400 text-xs font-medium">Notes</label>
+              <div className="bg-zinc-800/40 rounded-lg pl-3 pr-1 pt-3 pb-2 focus-within:ring-1 focus-within:ring-zinc-700 transition-all duration-200 max-h-22 overflow-auto">
+                <MobileAutoTextarea
+                  value={localNote}
+                  onChange={(e) =>
                     onAction({
-                      type: "changeScore",
-                      payload: newScore,
+                      type: "changeNote",
+                      payload: e.target.value,
                     })
                   }
+                  onBlur={() => onAction({ type: "saveNote" })}
+                  placeholder="Add your thoughts about this show..."
+                  className="w-full bg-transparent text-zinc-200 text-sm leading-relaxed resize-none outline-none placeholder-zinc-500"
                 />
               </div>
             </div>
-            <div className="text-zinc-400 text-sm -mt-1 flex items-center gap-2">
-              <span>{show.studio || "Unknown"}</span>•
-              <span>{show.dateReleased || "-"}</span>
-              {show.dateCompleted && (
-                <>
-                  •<span>{formatDateShort(show.dateCompleted)}</span>
-                </>
-              )}
-            </div>
-          </div>
-          {/* PROGRESS BAR */}
-          <div
-            className="mt-4.5 w-full bg-zinc-800/80 rounded-md h-1.5 overflow-hidden"
-            onClick={() => setIsProgressPickerOpen(true)}
-          >
-            <div
-              className={`${getStatusBg(
-                show.status
-              )} h-1.5 transition-all duration-500 ease-out rounded-md`}
-              style={{
-                width: `${
-                  show.seasons?.[show.curSeasonIndex]?.episode_count
-                    ? calcCurProgress(
-                        show.seasons,
-                        show.curSeasonIndex,
-                        show.curEpisode
-                      )
-                    : 0
-                }%`,
-              }}
-            />
-          </div>
-          {/* SEASON / EPISODES */}
-          <div className="mt-1 flex justify-between text-zinc-400 text-sm font-bold mb-0.5">
-            <span onClick={() => setIsProgressPickerOpen(true)}>
-              Season: {show.curSeasonIndex + 1 || "-"}
-            </span>
-            <span onClick={() => setIsProgressPickerOpen(true)}>
-              Episode: {show.curEpisode || "-"}
-            </span>
-          </div>
-          {/* STATUS */}
-          <div className="mt-2.5" data-no-drag>
-            <label className="text-zinc-400 text-xs font-medium">Status</label>
-            <div className="pt-1 flex flex-wrap gap-2 pb-1">
-              {showStatusOptions.map((status, index) => (
-                <button
-                  key={status.value}
-                  onClick={() =>
-                    onAction({
-                      type: "changeStatus",
-                      payload: `${status.label}`,
-                    })
-                  }
-                  className={`${
-                    index === 3 ? "w-full" : "flex-1"
-                  } px-4 py-1.5 text-sm rounded-md border border-zinc-700/30 font-semibold whitespace-nowrap transition-all duration-200 active:scale-95 ${
-                    status.label === show.status
-                      ? `${getStatusBg(status.label)} text-zinc-100`
-                      : "text-zinc-300 bg-zinc-900/40 hover:bg-zinc-800/60"
-                  }`}
-                >
-                  {status.label}
-                </button>
-              ))}
-            </div>
-          </div>
-          {/* NOTE */}
-          <div className="mt-1" data-no-drag>
-            <label className="text-zinc-400 text-xs font-medium">Notes</label>
-            <div className="bg-zinc-800/40 rounded-lg pl-3 pr-1 pt-3 pb-2 focus-within:ring-1 focus-within:ring-zinc-700 transition-all duration-200 max-h-22 overflow-auto">
-              <MobileAutoTextarea
-                value={localNote}
-                onChange={(e) =>
-                  onAction({
-                    type: "changeNote",
-                    payload: e.target.value,
-                  })
-                }
-                onBlur={() => onAction({ type: "saveNote" })}
-                placeholder="Add your thoughts about this show..."
-                className="w-full bg-transparent text-zinc-200 text-sm leading-relaxed resize-none outline-none placeholder-zinc-500"
-              />
-            </div>
           </div>
         </div>
-        <MobileProgressPicker
-          isOpen={isProgressPickerOpen}
-          seasons={show.seasons || []}
-          curSeasonIndex={show.curSeasonIndex}
-          curEpisode={show.curEpisode}
-          onClose={() => setIsProgressPickerOpen(false)}
-          onProgressChange={(seasonIndex, episode) => {
-            onAction({
-              type: "changeProgress",
-              payload: { seasonIndex, episode },
-            });
-            setIsProgressPickerOpen(false);
-          }}
-        />
       </div>
-    </div>
+      <MobileProgressPicker
+        isOpen={isProgressPickerOpen}
+        seasons={show.seasons || []}
+        curSeasonIndex={show.curSeasonIndex}
+        curEpisode={show.curEpisode}
+        onClose={() => setIsProgressPickerOpen(false)}
+        onProgressChange={(seasonIndex, episode) => {
+          onAction({
+            type: "changeProgress",
+            payload: { seasonIndex, episode },
+          });
+          setIsProgressPickerOpen(false);
+        }}
+      />
+    </>
   );
 }

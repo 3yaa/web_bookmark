192552944c4f5a2b793f5ec4b8ac7162678ac9d5
3yaa <3yaa@users.noreply.github.com>
2025-12-24 21:56:31 -0800

    um oppsies

--- app/movies/components/listingViews/MovieMobileListing.tsx
+++ app/movies/components/listingViews/MovieMobileListing.tsx
@@ -15,7 +15,7 @@ import React, { useRef, useState } from "react";
 import { MediaStatus } from "@/types/media";
 import { BackdropImageMobile } from "@/app/components/ui/BackdropMobile";
 import { useNav } from "@/app/components/NavContext";
-import { useVirtualizer } from "@tanstack/react-virtual";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 interface MovieMobileListingProps {
   movies: MovieProps[];
@@ -177,9 +177,8 @@ export function MovieMobileListing({
   const [openStatusOption, setOpenStatusOption] = useState(false);
   const parentRef = useRef<HTMLDivElement>(null);
 
-  const rowVirtualizer = useVirtualizer({
+  const rowVirtualizer = useWindowVirtualizer({
     count: movies.length,
-    getScrollElement: () => parentRef.current,
     estimateSize: () => 136,
     overscan: 5,
     measureElement: (element) => element?.getBoundingClientRect().height ?? 136,
@@ -195,7 +194,7 @@ export function MovieMobileListing({
   };
 
   return (
-    <div className="w-full mx-auto tracking-tight flex flex-col h-screen">
+    <div className="w-full mx-auto tracking-tight ">
       {/* HEADING */}
       <div className="sticky right-0 left-0 top-0 z-10 bg-zinc-900/35 backdrop-blur-xl shadow-lg border-b border-zinc-700/20 select-none flex justify-between items-center rounded-b-md will-change-transform">
         {/* STATUS FILTER */}
@@ -396,7 +395,7 @@ export function MovieMobileListing({
       )}
       {/* LISTING */}
       {!isProcessingMovie && movies.length > 0 && (
-        <div ref={parentRef} className="w-full overflow-auto flex-1">
+        <div ref={parentRef} className="w-full">
           <div
             style={{
               height: `${rowVirtualizer.getTotalSize()}px`,
